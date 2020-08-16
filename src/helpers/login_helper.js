b0bef73df55daa3e5f17a81db310b46b6929ac5f
Nikhil <nikhil051097>
2020-08-17 01:53:08 +0530

    changed backend url

--- src/helpers/login_helper.js
+++ src/helpers/login_helper.js
@@ -1,1 +1,9 @@
-export const isLoggedIn = () => window.localStorage["admin"] && JSON.parse(window.localStorage["lastLoggedin"]) + 86400000 * 2 > new Date().getTime() ? true : false; 
+export const isLoggedIn = () => {
+    let loggedIn = false;
+    try {
+        loggedIn = window.localStorage["admin"] && JSON.parse(window.localStorage["lastLoggedin"]) + 86400000 * 2 > new Date().getTime()
+    } catch (e) {
+        loggedIn = false;
+    }
+    return loggedIn;
+}
